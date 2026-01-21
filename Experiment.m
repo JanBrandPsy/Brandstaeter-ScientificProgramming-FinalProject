07d59c198bf95f7e5affeaa5d99c7951ed8f5c40
Jan <jan.brandstaeter@stud.plus.ac.at>
2026-01-21 11:06:05 +0100

    Initial Test Commit

---
+++ Experiment.m
@@ -0,0 +1,5 @@
+Screen('Preference', 'SkipSyncTests', 1); %Basics
+myScreen = 0;
+myBackgroundColour = [255 255 255];
+%myWindow = Screen('OpenWindow',myScreen, myBackgroundColour, [0 0 1920 1080]);
+myWindow = Screen('OpenWindow',myScreen, myBackgroundColour, [0 0 1280 720]);
